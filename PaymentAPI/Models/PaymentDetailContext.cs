07179333d3f73a28aa6b378593a4a562cebfc59b
jaouad mhamdi <98467197+jm-hamdi@users.noreply.github.com>
2024-09-20 16:59:41 +0100

    PaymentAPI

---
+++ PaymentAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions options): base(options)
+        {
+        }
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
